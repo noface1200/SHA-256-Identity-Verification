13e1b1c9e13994c65edbc7ac01c49faca1e74d90
Noface <166717111+noface1200@users.noreply.github.com>
2025-03-20 00:20:19 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import hashlib, argparse
+
+parser = argparse.ArgumentParser(description="sha256 identity verification tool")
+
+def hash(input_string):
+    hash_object = hashlib.sha256(input_string.encode())
+    return hash_object.hexdigest()
+
+def create_master_key(passwords):
+    password_list = []
+    for password in passwords:
+        password_list.append(hash(password))
+    return ":".join(password_list)
+
+def verify(master, password):
+    hashes = master.split(':')
+    password_hash = hash(password)
+    for hash_obj in hashes:
+        if hash_obj == password_hash:
+            return True
+    return False
+
+parser.add_argument('--verify', nargs=2, metavar=('MASTER', 'PASSWORD'), help='verify key owners identity with key and password')
+parser.add_argument('--keygen', nargs='+', metavar='VALUE', help='Generate a key from a list of passwords')
+
+args = parser.parse_args()
+
+if args.verify:
+    master, password = args.verify
+    print(str(verify(master, password)))
+
+elif args.keygen:
+    print(create_master_key(args.keygen))
+else:
+    print('SHA256 Identity Verifier\n\nOptions:\n--verify {master_key} {password}  Verify identity with the provided master key and password.\n  Example: python script.py --verify >
